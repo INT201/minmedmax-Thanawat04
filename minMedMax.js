73cab141011a4a8c8cf753949f021e7594684646
Thanawat Chen <thanawat.th@mail.kmutt.ac.th>
2022-09-10 13:42:18 +0700

    v1

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  if (n1>n2&&n3)
+    return`max: ${n1}`
+  else if (n2>n3)
+    return `med: ${n2},min: ${n3}`
+  else 
+    return `med: ${n3},min: ${n2}`
+
+  
+
 }
 module.exports = minMedMax
